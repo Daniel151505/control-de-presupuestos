502fc57febaf60ea96666e47e4bbd5c8631ebd93
Daniel151505 <daniel150520011@gmail.com>
2023-02-05 19:01:25 -0500

    Mostrando el componente de Gastos si el presupuesto es valido

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,28 @@
-import React from 'react'
-import NuevoPresupuesto from './NuevoPresupuesto'
+import React from "react";
+import NuevoPresupuesto from "./NuevoPresupuesto";
 
-const Header = ({presupuesto, setPresupuesto}) => {
+const Header = ({
+  presupuesto,
+  setPresupuesto,
+  isValidPresupuesto,
+  setIsValidPresupuesto,
+}) => {
   return (
     <header>
-        <h1>Planificador de Presupuesto</h1>  
+      <h1>Planificador de Presupuesto</h1>
+
+      { isValidPresupuesto ? (
+        <p>Control Presupuesto</p>
+      ): (
         <NuevoPresupuesto
           presupuesto={presupuesto}
           setPresupuesto={setPresupuesto}
+          setIsValidPresupuesto={setIsValidPresupuesto}
         />
+      )}
+      
     </header>
+  );
+};
 
-  )
-}
-
-export default Header
+export default Header;
