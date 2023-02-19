6e77b7eeb707375076f65851e1128d943695dcce
Daniel151505 <daniel150520011@gmail.com>
2023-02-19 17:16:40 -0500

    Detectando el gasto a Editar al dar Swipe

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Header from "./components/Header";
 import ListadoGastos from "./components/ListadoGastos";
 import Modal from "./components/Modal";
@@ -6,37 +6,45 @@ import { generarId } from "./helpers";
 import IconoNuevoGasto from "./img/nuevo-gasto.svg";
 
 function App() {
-  const [gastos, setGastos] = useState([])
+  const [gastos, setGastos] = useState([]);
 
   const [presupuesto, setPresupuesto] = useState(0);
   const [isValidPresupuesto, setIsValidPresupuesto] = useState(false);
 
-  const [modal, setModal] = useState(false)
-  const [animarModal, setAnimarModal] = useState(false)
+  const [modal, setModal] = useState(false);
+  const [animarModal, setAnimarModal] = useState(false);
+
+  const [gastoEditar, setGastoEditar] = useState({});
+
+  useEffect(() => {
+    if (Object.keys(gastoEditar).length > 0) {
+      handleNuevoGasto();
+    }
+  }, [gastoEditar]);
 
   const handleNuevoGasto = () => {
-    setModal(true)
+    setModal(true);
 
     setTimeout(() => {
-      setAnimarModal(true)
+      setAnimarModal(true);
     }, 500);
-  }
+  };
 
-  const guardarGasto = gasto => {
-    gasto.id = generarId()
+  const guardarGasto = (gasto) => {
+    gasto.id = generarId();
     gasto.fecha = Date.now();
-    setGastos([...gastos, gasto])
+    setGastos([...gastos, gasto]);
 
     setAnimarModal(false);
     setTimeout(() => {
       setModal(false);
     }, 500);
-  }
+  };
 
   return (
-    <div className={modal ? 'fijar' : ''} >
+    <div className={modal ? "fijar" : ""}>
       <Header
-        gastos= {gastos}
+        gastos={gastos}
         presupuesto={presupuesto}
         setPresupuesto={setPresupuesto}
         isValidPresupuesto={isValidPresupuesto}
@@ -46,24 +54,26 @@ function App() {
       {isValidPresupuesto && (
         <>
           <main>
-            <ListadoGastos
-              gastos= {gastos}
-            />
+            <ListadoGastos gastos={gastos} setGastoEditar={setGastoEditar} />
           </main>
           <div className="nuevo-gasto">
-            <img src={IconoNuevoGasto} alt="Icono nuevo Gasto" onClick={handleNuevoGasto} />
+            <img
+              src={IconoNuevoGasto}
+              alt="Icono nuevo Gasto"
+              onClick={handleNuevoGasto}
+            />
           </div>
         </>
       )}
 
-      {modal && <Modal 
-                  setModal={setModal} 
-                  animarModal={animarModal} 
-                  setAnimarModal={setAnimarModal} 
-                  guardarGasto={guardarGasto}
-                />
-      }
-
+      {modal && (
+        <Modal
+          setModal={setModal}
+          animarModal={animarModal}
+          setAnimarModal={setAnimarModal}
+          guardarGasto={guardarGasto}
+        />
+      )}
     </div>
   );
 }
--- src/components/ListadoGastos.jsx
+++ src/components/ListadoGastos.jsx
@@ -1,20 +1,16 @@
-import React from 'react'
-import Gasto from './Gasto'
+import React from "react";
+import Gasto from "./Gasto";
 
-const ListadoGastos = ({gastos}) => {
+const ListadoGastos = ({ gastos, setGastoEditar }) => {
   return (
-    <div className='listado-gastos contenedor'>
-        <h2> {gastos.length ? 'Gastos' : 'No hay gastos aún'} </h2>
+    <div className="listado-gastos contenedor">
+      <h2> {gastos.length ? "Gastos" : "No hay gastos aún"} </h2>
 
-        {gastos.map( gasto => (
-            <Gasto
-                key={gasto.id}
-                gasto= {gasto}
-            />
-        ))}
-
+      {gastos.map((gasto) => (
+        <Gasto key={gasto.id} gasto={gasto} setGastoEditar={setGastoEditar} />
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default ListadoGastos
+export default ListadoGastos;
