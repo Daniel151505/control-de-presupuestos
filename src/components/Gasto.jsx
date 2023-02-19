79a555a306d4b1c580a0f729a2e0920f80a67c8d
Daniel151505 <daniel150520011@gmail.com>
2023-02-18 21:28:15 -0500

    Formateando las fechas

--- src/components/Gasto.jsx
+++ src/components/Gasto.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import { formatearFecha } from "../helpers";
 
 const Gasto = ({ gasto }) => {
-  const { categoria, nombre, cantidad, id } = gasto;
+  const { categoria, nombre, cantidad, id, fecha } = gasto;
 
   return (
     <div className="gasto sombra">
@@ -9,8 +10,12 @@ const Gasto = ({ gasto }) => {
         <div className="descripcion-gasto">
           <p className="categoria">{categoria}</p>
           <p className="nombre-gasto">{nombre}</p>
+          <p className="fecha-gasto">
+            Agregado el: {""} <span> {formatearFecha(fecha)} </span>{" "}
+          </p>
         </div>
       </div>
+      <p className="cantidad-gasto"> S/.{cantidad} </p>
     </div>
   );
 };
