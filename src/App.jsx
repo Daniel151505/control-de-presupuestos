1b184e6b943c5b5aadc2c7ca546944523afc5037
Daniel151505 <daniel150520011@gmail.com>
2023-02-21 20:41:36 -0500

    Colocar el presupuesto en LocalStorage

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,9 @@ import IconoNuevoGasto from "./img/nuevo-gasto.svg";
 function App() {
   const [gastos, setGastos] = useState([]);
 
-  const [presupuesto, setPresupuesto] = useState(0);
+  const [presupuesto, setPresupuesto] = useState(
+    Number(localStorage.getItem("presupuesto")) ?? 0
+  );
   const [isValidPresupuesto, setIsValidPresupuesto] = useState(false);
 
   const [modal, setModal] = useState(false);
@@ -34,6 +36,18 @@ function App() {
       setAnimarModal(true);
     }, 500);
   };
+
+  useEffect(() => {
+    localStorage.setItem("presupuesto", presupuesto ?? 0);
+  }, [presupuesto]);
+
+  useEffect(() => {
+    const presupuestoLS = Number(localStorage.getItem("presupuesto")) ?? 0;
+
+    if (presupuestoLS > 0) {
+      setIsValidPresupuesto(true);
+    }
+  }, []);
 
   const guardarGasto = (gasto) => {
     if (gasto.id) {
